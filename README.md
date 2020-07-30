4ddc4c65d85fd5b2f1a0a649fa1c8ec2fc2e2209
Jeff Deards <jdeards84@gmail.com>
2020-07-30 00:39:42 -0700

    modified and cleaned up html, javascript, and css.  Updated images and added README.md and screenshots

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # PurplePress App
 
-This is a app for my Bloc Capstone project.
+A news app which allows users to search for specific topics, or top news of the day, and displays article results based on the article source's bias.
+
+## Summary
+
+PurplePress allows users to access news articles, based on any given search term, by utilizing the News API (https://newsapi.org/), along with the Unsplash API (https://unsplash.com/documentation#search-photos), and returning 3 lists of results representing conservative, liberal, and neutral media sources.  Users are then able to peruse each list, as well as learn about how the results are determined.  
+
+## Screenshots
+Search Screen/Landing Page:
+
+![search screen](screenshots/search.png)
+
+About:
+
+![about](screenshots/about.png)
+
+Results:
+
+![library](screenshots/results.png)
+
+Error:
+
+![recommendations](screenshots/error.png)
+
+## Technologies 
+
+HTML/CSS/JavaScript/jQuery
+
+## Demo
+
+- [Live Demo](https://jdeardsjr.github.io/PurplePressApp/)
+
--- index.js
+++ index.js
@@ -4,29 +4,35 @@ const unsplashAccessKey = 'x4OFHJL9iYcRYRKo6HjH4Mm6yQOrlqVgYUtVQLVvTfk';
 
 const unsplashUrl = 'https://api.unsplash.com/search/photos';
 
-//const apiKey = 'e5c6e69222df499d91b50804ec7f256d';
+const rapidApiHost = 'bing-news-search1.p.rapidapi.com';
 
-//const searchUrl = 'https://api.cognitive.microsoft.com/bing/v7.0/news/search';
+const rapidApiKey = '83e4845796msh90430094bea1322p1c0a2bjsn3008c6d1999f';
 
-const searchUrl = 'https://bing-news-search1.p.rapidapi.com/news/search'
+const searchUrl = 'https://bing-news-search1.p.rapidapi.com/news/search';
 
+const defaultImage = 'images/defaultBackgroundA.jpg';
+
+// sets alternating placeholder
 const searchEx = [ 'Enter Topic', '~ OR ~', 'Click "Go!" for Top Stories!' ];
   setInterval(function() {
-    $("#js-search-topic").attr("placeholder", searchEx[searchEx.push(searchEx.shift())-1]);
+    $('#js-search-topic').attr('placeholder', searchEx[searchEx.push(searchEx.shift())-1]);
   }, 2000);
 
-/*$(document).ready(function(){
-  if($(window).width >= 700) {
-    $('#blue-scroller').removeClass('hidden');
-  }
-});*/
-
-function renderAboutHowFooter() {
-  $('.js-about').removeClass('hidden');
-  $('.js-how').removeClass('hidden');
-  $('.js-footer').removeClass('hidden');
+// generates error html and passes in search term to google search link
+function errorHtml(searchTerm) {
+  $('.js-error-container').html(
+      `<section class="error-container">
+        <h2 id="js-error-message" class="error-message logo-styling">Oops!</h2> 
+        <p>Your search cannot be completed as currently entered.  Please check spelling and re-phrase if possible.  Try again!</p>
+        <h3 class="p-margin logo-styling">Still No Luck?</h3>  
+        <a class="google-link" href="https://www.google.com/search?q=${searchTerm}" target="_blank">BROWSE GOOGLE'S SEARCH RESULTS</a>
+      </section>`
+  );
+  console.log('error message ran');
+  window.location.href = '#js-form';
 }
 
+// determines which article image to display and generates purple article html
 function purpleHtml(purpleArticle) {
   let purpleArticleImage = '';
   if (purpleArticle.image) {
@@ -41,7 +47,7 @@ function purpleHtml(purpleArticle) {
         <h3><a href="${purpleArticle.url}" target="_blank" class="purple article-name">${purpleArticle.name}</a></h3>
       </div>
       <div>
-        <p class="purple-article-source">By ${purpleArticle.provider[0].name}</p>
+        <p class="purple-article-source">${purpleArticle.provider[0].name}</p>
         <p class="article-description">${purpleArticle.description}</p>
       </div>
     </li>`
@@ -49,6 +55,7 @@ function purpleHtml(purpleArticle) {
   console.log('`purpleHtml` ran')
 }
 
+// determines which article image to display and generates blue article html
 function blueHtml(blueArticle) {
   let blueArticleImage = '';
   if (blueArticle.image) {
@@ -63,7 +70,7 @@ function blueHtml(blueArticle) {
         <h3><a href="${blueArticle.url}" target="_blank" class="blue article-name">${blueArticle.name}</a></h3>
       </div>
       <div>
-        <p class="blue-article-source">By ${blueArticle.provider[0].name}</p>
+        <p class="blue-article-source">${blueArticle.provider[0].name}</p>
         <p class="article-description">${blueArticle.description}</p>
       </div>
     </li>`
@@ -71,117 +78,218 @@ function blueHtml(blueArticle) {
   console.log('`blueHtml` ran');
 }
 
+// determines which article image to display and generates red article html
 function redHtml(redArticle) {
-  let redArticleImage = '';
-  if (redArticle.image) {
-    redArticleImage = redArticle.image.thumbnail.contentUrl;
-  } else if (redArticle.provider[0].image) {
-    redArticleImage = redArticle.provider[0].image.thumbnail.contentUrl;
-  } 
-  $('#red-results-list').append(
-    `<li>
-      <div class="image-title-container">
-        <img src="${redArticleImage}" class="results-img" alt="Article Image">
-        <h3 class="article-name"><a class="red article-name" href="${redArticle.url}" target="_blank">${redArticle.name}</a></h3>
-      </div>
-      <div>
-        <p class="article-source">${redArticle.provider[0].name}</p>
-        <p class="article-description">${redArticle.description}</p>
-      </div>
-    </li>`
-  );  
+    let redArticleImage = '';
+    if (redArticle.image) {
+      redArticleImage = redArticle.image.thumbnail.contentUrl;
+    } else if (redArticle.provider[0].image) {
+      redArticleImage = redArticle.provider[0].image.thumbnail.contentUrl;
+    } 
+    $('#red-results-list').append(
+      `<li>
+        <div class="image-title-container">
+          <img src="${redArticleImage}" class="results-img" alt="Article Image">
+          <h3 class="article-name"><a class="red article-name" href="${redArticle.url}" target="_blank">${redArticle.name}</a></h3>
+        </div>
+        <div>
+          <p class="article-source">${redArticle.provider[0].name}</p>
+          <p class="article-description">${redArticle.description}</p>
+        </div>
+      </li>`
+    );
   console.log('`redHtml` ran');
 }
 
-function emptyAbout() {
-  $('.js-about-button').addClass('hidden');
-  $('.js-about').addClass('hidden');
+function emptyErrorMessage() {
+  $('.js-error-container').empty();
 }
 
+// hides about button and container on submit click
+function handleAbout() {
+  $('.js-about-button').addClass('hidden');
+  $('.js-about-container').addClass('hidden');
+}
+
+// displays previous search button 
+function handlePrevious() {
+  $('.js-previous-button').removeClass('hidden');
+}
+
+// displays, and screen jumps to, results container
+function handleResultsContainer() {
+  $('.container').removeClass('hidden')
+  window.location.href = '#container';
+}
+
+// empties previous results
 function emptyResults() {
   $('#purple-results-list').empty();
   $('#red-results-list').empty();
   $('#blue-results-list').empty();
 }
 
-function emptyErrorMessage() {
-  $('.error-message').empty();
+// displays about container and footer
+function renderAboutFooter() {
+  $('.js-about-container').removeClass('hidden');
+  $('.js-footer').removeClass('hidden');
 }
 
+// jumps to about container 
+function handleScreenJump() {
+  if (!$('#about-container').hasClass('hidden')) {
+    window.location.href = '#about-container';
+  };
+}
+
+// ensures .fa-search has blink class, un-rotates .press-border and displays footer and buttons
+function handleFooterButtons() {
+  $('.fa-search').addClass('blink');
+  $('.press-border').removeClass('rotated');
+  $('.js-footer-button').removeClass('hidden');
+  $('.js-footer').removeClass('hidden');
+}
+
+// hides .js-about-button and toggles hidden attribute on .js-about-container
+// toggles blink and rotated classes on .press-border and .fa-search buttons
+function handleFooterToggle() {
+  $('.js-about-button').addClass('hidden');
+  $('.js-about-container').toggleClass('hidden');
+  $('.press-border').toggleClass('rotated', 'blink');
+  $('.fa-search').toggleClass('blink');
+}
+
+// determines if there is a search term to pass to the image api
+// if not, it renders the default image
+function handleImageQuery(searchTerm) {
+  if (searchTerm === '') {
+    $('.results').css('background-image', 'linear-gradient(black, black), url(' + defaultImage + ')');
+  } else {
+    getImage(searchTerm);
+  }
+}
+
+// assigns color seartch term variables applies specific site parameters to each
+// assigns queries array and passes it, along with the initial search term, to getNews function
 function getColorNews(searchTerm) {
   const purpleSearchTerm = `${searchTerm} (site:reuters.com OR site:apnews.com OR site:bloomberg.com OR site:ABCNews.go.com OR site:npr.org OR site:wsj.com OR site:chicagotribune.com OR site:cbsnews.com OR site:usatoday.com)`;
   const redSearchTerm = `${searchTerm} (site:foxnews.com OR site:washingtontimes.com OR site:nationalreview.com OR site:spectator.org OR site:townhall.com OR site:conservativereview.com)`;
   const blueSearchTerm = `${searchTerm} (site:cnn.com OR site:msnbc.com OR site:thedailybeast.com OR site:vanityfair.com OR site:counterpunch.org OR site:washingtonmonthly.com)`;
-  getNews(purpleSearchTerm);
-  getNews(redSearchTerm);
-  getNews(blueSearchTerm);
+  const queries = [redSearchTerm, blueSearchTerm, purpleSearchTerm]
+  getNews(queries, searchTerm);
 }
 
+// maps over parameters and returns them
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
+// determines if unsplash API responseJson returned an image result and, if not, displays default image.  Otherwise, it displays returned image.
 function displayImageResults(responseJson) {
   console.log(responseJson);
-  const imageUrl = responseJson.results[0].urls.regular;
-  $('.container').css('background-image', 'linear-gradient(black, black), url(' + imageUrl + ')');
+    if (responseJson.total === 0) {
+      $('.results').css('background-image', 'linear-gradient(black, black), url(' + defaultImage + ')');
+    } 
+    const imageUrl = responseJson.results[0].urls.regular;
+    $('.results').css('background-image', 'linear-gradient(black, black), url(' + imageUrl + ')');
 }
 
-function displayResults(responseJson) {
-  console.log(responseJson);
+// empties any previous article results
+// loops through newResponseJson and sends each item to corresponding color html function
+// ensures results container and previous search button are displayed
+function displayResults(newResponseJson) {
+  console.log(newResponseJson);
 
-  for (let i = 0; i < responseJson.value.length; i++) {
-    if (responseJson.value[i].provider[0].name === 'Associated Press' || responseJson.value[i].provider[0].name === 'ABC on MSN.com' || responseJson.value[i].provider[0].name === 'ABC' || responseJson.value[i].provider[0].name === 'ABCNews' || responseJson.value[i].provider[0].name === 'NPR' || responseJson.value[i].provider[0].name === 'Reuters' || responseJson.value[i].provider[0].name === 'Wall Street Journal' || responseJson.value[i].provider[0].name === 'Bloomberg' || responseJson.value[i].provider[0].name === 'Chicago Tribune' || responseJson.value[i].provider[0].name === 'CBS News' || responseJson.value[i].provider[0].name === 'USA Today') {
-      const purpleArticle = responseJson.value[i];
-      purpleHtml(purpleArticle);
-    } else if (responseJson.value[i].provider[0].name === 'CNN' || responseJson.value[i].provider[0].name === 'CNN on MSN.com' || responseJson.value[i].provider[0].name === 'cnn.com' || responseJson.value[i].provider[0].name === 'MSNBC' || responseJson.value[i].provider[0].name === 'CounterPunch' || responseJson.value[i].provider[0].name === 'Vanity Fair' || responseJson.value[i].provider[0].name === 'The Daily Beast'|| responseJson.value[i].provider[0].name === 'Washington Monthly') {
-      const blueArticle = responseJson.value[i];
-      blueHtml(blueArticle);
-    } else if (responseJson.value[i].provider[0].name === 'Fox News' ||  responseJson.value[i].provider[0].name === 'National Review' || responseJson.value[i].provider[0].name === 'Washington Times' || responseJson.value[i].provider[0].name === 'The American Spectator' ||  responseJson.value[i].provider[0].name === 'Townhall' || responseJson.value[i].provider[0].name === 'Conservative Review') {
-      const redArticle = responseJson.value[i];
-      redHtml(redArticle);
-    } 
+  emptyResults();
+  for (let i = 0; i < newResponseJson.length; i++) {
+    if (newResponseJson[i] === newResponseJson[0]) {
+      for (let i = 0; i < newResponseJson[0].value.length; i++) {
+        const redArticle = newResponseJson[0].value[i];
+        redHtml(redArticle);
+      }
+    } else if (newResponseJson[i] === newResponseJson[1]) {
+      for(let i = 0; i < newResponseJson[1].value.length; i++) {
+        const blueArticle = newResponseJson[1].value[i];
+        blueHtml(blueArticle); 
+      }
+    } else if (newResponseJson[i] === newResponseJson[2]) {
+      for (let i = 0; i < newResponseJson[2].value.length; i++) {
+        const purpleArticle = newResponseJson[2].value[i];
+        purpleHtml(purpleArticle);
+      }
+    }
   }
-  //if (responseJson.value.length !== 0) {
-  $('.container').removeClass('hidden')
-  window.location.href = '#container';
-  //}
+  handleResultsContainer();
+  handlePrevious();
   console.log('`displayResults` ran');
 };
 
-function getNews(query) {
-  const params = {
-    q: query,
-    count: 3,
-    sortBy: 'relevance'
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchUrl + '?' + queryString;
-
-  console.log(url);
-
-  const options = {
-    headers: new Headers({
-      "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
-      "x-rapidapi-key": "83e4845796msh90430094bea1322p1c0a2bjsn3008c6d1999f",
-      "x-bingapis-sdk": "true"})
-  };
-
-  fetch(url, options)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+// determines if newResponseJson returned results for all three categories and, if so, runs displayResults function
+// if not, it runs errorHtml
+function handleNewResponse(newResponseJson, searchTerm) {
+  if (newResponseJson.length === 3) {
+    displayResults(newResponseJson);
+  } else {
+    errorHtml(searchTerm);
+  }
 }
 
+// loops through responseJson from article search and pushes any items with a length other than 0 to newResponseJson
+// runs handleNewResponse function
+function createNewResponse(responseJson, searchTerm) {
+  //console.log(responseJson);
+
+  let newResponseJson = [];
+  for (let i = 0; i < responseJson.length; i++) {
+    if (responseJson[i].value.length !== 0) {
+      newResponseJson.push(responseJson[i]);
+    } 
+  }
+  handleNewResponse(newResponseJson, searchTerm);
+}
+
+// loops through queries and applies params to each, then pushes urls to requests variable
+// makes promise.all fetch call to news api and handles response
+function getNews(queries, searchTerm) {
+  const options = {
+    headers: new Headers({
+      'x-rapidapi-host': rapidApiHost,
+      'x-rapidapi-key': rapidApiKey,
+      'x-bingapis-sdk': 'true'})
+  };
+
+  let requests = [];
+  for (let i = 0; i < queries.length; i++) {
+    const params = {
+      q: queries[i],
+      count: 3,
+      sortBy: 'relevance'
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchUrl + '?' + queryString;
+
+    console.log(url);
+
+ 
+    requests.push(fetch(url, options));
+  }
+
+  Promise.all(requests)
+    .then(responses => Promise.all(responses.map(r => r.json())))
+
+    .then(responseJson => createNewResponse(responseJson, searchTerm))
+
+    .catch(err => {
+      //$('.js-error-container').html(`<p id="js-error-message" class="error-message">Something went wrong: your search cannot be completed as currently entered.  Please check spelling and try again!</p>`);
+      errorHtml(searchTerm);
+      console.log(err);
+    });
+  
+}
+
+// assigns params to query and passes url into fetch call.  Handles response
 function getImage(query) {
   const params = {
     client_id: unsplashAccessKey,
@@ -195,51 +303,61 @@ function getImage(query) {
 
   fetch(url)
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
+      return response.json();
     })
     .then(responseJson => displayImageResults(responseJson))
     .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-      //$('.error-container').html(`<p id="js-error-message" class="error-message">Something went wrong: your search cannot be completed as currently entered.  Please check spelling and try again!</p>`);
+      console.log(err);
     });
 }
 
+// handles submit button click
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-topic').val();
-    emptyAbout();
-    emptyResults();
     emptyErrorMessage();
     getColorNews(searchTerm);
-    getImage(searchTerm);
+    handleImageQuery(searchTerm)
+    handleAbout();
+    handleFooterButtons();
   });
 }
 
+// handles about button click
 function watchAbout() {
   $('.js-about-button').on('click', function(event) {
-    renderAboutHowFooter();
+    renderAboutFooter();
+    handleScreenJump();
   });
 }
 
+// handles red article toggle click
 function watchRedToggle() {
   $('.js-red-button').on('click', function(event) {
-    $('#red-scroller').toggle('.hidden');
+    $('#red-scroller').toggleClass('hidden');
   });
 }
 
+// handles blue article toggle click
 function watchBlueToggle() {
   $('.js-blue-button').on('click', function(event) {
-    $('#blue-scroller').toggle('.hidden');
+    $('#blue-scroller').toggleClass('hidden');
   });
 }
 
+// handles purple article toggle click
 function watchPurpleToggle() {
   $('.js-purple-button').on('click', function(event) {
-    $('#purple-scroller').toggle('.hidden');
+    $('#purple-scroller').toggleClass('hidden');
+  });
+}
+
+// handles footer toggle click
+function watchFooterToggle() {
+  $('.js-footer-button').on('click', function(event) {
+    handleFooterToggle();
+    handleScreenJump();
   });
 }
 
@@ -249,6 +367,7 @@ function handlePurplePress() {
   watchRedToggle();
   watchBlueToggle();
   watchPurpleToggle();
+  watchFooterToggle();
 }
 
 $(handlePurplePress);
